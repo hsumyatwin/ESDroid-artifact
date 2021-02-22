a5c198deec55b381fa6d3adc56f3d87948ab5254
Hsu Myat Win <43127275+hsumyatwin@users.noreply.github.com>
2021-02-22 19:25:01 +1100

    Add files via upload

---
+++ test-data/Bites/DD/14.py
@@ -0,0 +1,35 @@
+#start monkey test seedNo 0
+import os;
+from subprocess import Popen
+from subprocess import PIPE
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice, MonkeyImage
+from com.android.monkeyrunner.MonkeyDevice import takeSnapshot
+from com.android.monkeyrunner.easy import EasyMonkeyDevice
+from com.android.monkeyrunner.easy import By
+from com.android.chimpchat.hierarchyviewer import HierarchyViewer
+from com.android.monkeyrunner import MonkeyView
+import random
+import sys
+import subprocess
+from sys import exit
+from random import randint
+device = MonkeyRunner.waitForConnection()
+package = 'caldwell.ben.bites'
+activity ='caldwell.ben.bites.Bites'
+runComponent = package+'/'+activity
+device.startActivity(component=runComponent)
+MonkeyRunner.sleep(0.2)
+MonkeyRunner.sleep(0.2)
+device.touch(584,1014, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.2)
+device.touch(1070,1850, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.2)
+device.touch(1000,1862, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.2)
+device.touch(678,1645, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.2)
+device.touch(437,988, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.2)
+device.touch(338,1337, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.2)
+device.touch(443,798, 'DOWN_AND_UP')
