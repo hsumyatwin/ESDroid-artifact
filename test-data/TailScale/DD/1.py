e5a79d0cfdf29be9362053711a85d706af5736fe
Hsu Myat Win <43127275+hsumyatwin@users.noreply.github.com>
2021-07-08 18:57:10 +1000

    Add files via upload

---
+++ test-data/TailScale/DD/1.py
@@ -0,0 +1,45 @@
+#start monkey test seedNo 0
+import os;
+from subprocess import Popen
+from subprocess import PIPE
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice, MonkeyImage
+from com.android.monkeyrunner.MonkeyDevice import takeSnapshot
+from com.android.monkeyrunner.easy import EasyMonkeyDevice
+from com.android.monkeyrunner.easy import By
+from com.android.chimpchat.hierarchyviewer import HierarchyViewer
+from com.android.monkeyrunner import MonkeyView
+import random
+import sys
+import subprocess
+from sys import exit
+from random import randint
+device = MonkeyRunner.waitForConnection()
+package = 'com.tailscale.ipn'
+activity ='org.gioui.GioActivity'
+runComponent = package+'/'+activity
+device.startActivity(component=runComponent)
+MonkeyRunner.sleep(0.1)
+MonkeyRunner.sleep(0.1)
+device.touch(569,1625, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.1)
+device.touch(144,220, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.1)
+device.touch(1057,1320, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.1)
+device.touch(772,323, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.1)
+device.touch(790,491, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.1)
+device.touch(689,1024, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.1)
+device.touch(645,650, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.1)
+device.touch(416,1772, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.1)
+device.touch(950,1697, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.1)
+device.touch(297,298, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.1)
+device.touch(778,1282, 'DOWN_AND_UP')
+MonkeyRunner.sleep(0.1)
+device.touch(769,382, 'DOWN_AND_UP')
