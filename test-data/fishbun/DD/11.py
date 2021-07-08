e968dcb61aa0d6469d760e5186b7e5dda5bfed82
Hsu Myat Win <43127275+hsumyatwin@users.noreply.github.com>
2021-07-08 18:06:04 +1000

    Add files via upload

---
+++ test-data/fishbun/DD/11.py
@@ -0,0 +1,31 @@
+#start monkey test seedNo 0
+import os;
+from subprocess import Popen
+from subprocess import PIPE
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice, MonkeyImage
+from com.android.monkeyrunner.MonkeyDevice import takeSnapshot
+from com.android.monkeyrunner.easy import EasyMonkeyDevice
+from com.android.monkeyrunner.easy import By
+from com.android.chimpchat.hierarchyviewer import HierarchyViewer
+from com.android.monkeyrunner import MonkeyView
+import random
+import sys
+import subprocess
+from sys import exit
+from random import randint
+device = MonkeyRunner.waitForConnection()
+package = 'com.sangcomz.fishbundemo'
+activity ='com.sangcomz.fishbundemo.ChoiceActivity'
+runComponent = package+'/'+activity
+device.startActivity(component=runComponent)
+MonkeyRunner.sleep(1)
+MonkeyRunner.sleep(1)
+device.touch(652,1330, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(328,1153, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(250,1851, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(253,1862, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(992,441, 'DOWN_AND_UP')
---
+++ test-data/fishbun/DD/6.py
@@ -0,0 +1,39 @@
+#start monkey test seedNo 0
+import os;
+from subprocess import Popen
+from subprocess import PIPE
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice, MonkeyImage
+from com.android.monkeyrunner.MonkeyDevice import takeSnapshot
+from com.android.monkeyrunner.easy import EasyMonkeyDevice
+from com.android.monkeyrunner.easy import By
+from com.android.chimpchat.hierarchyviewer import HierarchyViewer
+from com.android.monkeyrunner import MonkeyView
+import random
+import sys
+import subprocess
+from sys import exit
+from random import randint
+device = MonkeyRunner.waitForConnection()
+package = 'com.sangcomz.fishbundemo'
+activity ='com.sangcomz.fishbundemo.ChoiceActivity'
+runComponent = package+'/'+activity
+device.startActivity(component=runComponent)
+MonkeyRunner.sleep(1)
+MonkeyRunner.sleep(1)
+device.touch(244,318, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(652,1330, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(328,1153, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(38,899, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(100,1300, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(469,1487, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(250,1851, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(253,1862, 'DOWN_AND_UP')
+MonkeyRunner.sleep(1)
+device.touch(992,441, 'DOWN_AND_UP')
